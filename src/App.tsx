9e2eb4573b5c1ae6be6c5428449b8594be048c32
alina <kostenkoalina97@icloud.com>
2025-09-04 16:57:23 +0200

    fis links

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import classNames from 'classnames';
 
-const getLinkClass = ({ isActive }: { isActive: boolean }) =>
-  classNames('navbar-item', {
-    'is-active': isActive,
-  });
+export const App = () => {
+  const { pathname } = useLocation();
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink to="/" className={getLinkClass}>
-            Home
-          </NavLink>
-          <NavLink to="/tabs" className={getLinkClass}>
-            Tabs
-          </NavLink>
+  const isHomeActive = pathname === '/';
+  const isTabsActive = pathname.startsWith('/tabs');
+
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <div className={`navbar-item${isHomeActive ? ' is-active' : ''}`}>
+              <Link to="/">Home</Link>
+            </div>
+            <div className={`navbar-item${isTabsActive ? ' is-active' : ''}`}>
+              <Link to="/tabs">Tabs</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Outlet />
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Outlet />
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
